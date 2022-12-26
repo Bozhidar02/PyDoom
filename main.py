e44f5d11580f9a384b640e18aaaa8c3574a0a655
Bozhidar Goranov <goranovbobo@gmail.com>
2022-12-26 10:07:39 +0200

    Added functions to run game

--- main.py
+++ main.py
@@ -9,4 +9,30 @@ class Game:
         self.screen = pygame.display.set_mode(Res)
         self.clock = pygame.time.Clock()
 
+    def new_game(self):
+        pass
 
+    def update_screen(self):
+        pygame.display.flip()
+        self.clock.tick(FPS)
+        pygame.display.set_caption(f'{self.clock.get_fps():.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                pygame.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update_screen()
+            self.draw()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
